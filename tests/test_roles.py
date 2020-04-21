7a5667897060d3ed3d16f3bed589dad4d181479b
Nathan Cahill <nathan@nathancahill.com>
2020-04-21 20:34:16 +0200

    add tests

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -2,7 +2,8 @@ from schemahq.roles import extract_roles
 
 
 def test_extract_roles():
-    roles, other = extract_roles("""
+    roles, other = extract_roles(
+        """
 CREATE ROLE admin;
 CREATE ROLE jonathan LOGIN;
 CREATE ROLE davide WITH PASSWORD 'jw8s0F4';
@@ -17,10 +18,13 @@ CREATE TABLE films (
     len         interval hour to minute
 );
 SELECT kind, sum(len) AS total FROM films GROUP BY kind;
-""")
+"""
+    )
 
     assert len(roles) == 5
-    assert other == """CREATE TABLE films (
+    assert (
+        other
+        == """CREATE TABLE films (
     code        char(5) CONSTRAINT firstkey PRIMARY KEY,
     title       varchar(40) NOT NULL,
     did         integer NOT NULL,
@@ -30,23 +34,26 @@ SELECT kind, sum(len) AS total FROM films GROUP BY kind;
 );
 SELECT kind, sum(len) AS total FROM films GROUP BY kind;
 """
+    )
 
 
 def test_roles():
     roles, _ = extract_roles("CREATE ROLE admin;")
-    role = roles['admin']
+    role = roles["admin"]
 
     assert role.name == "admin"
     assert role.password is None
 
     roles, _ = extract_roles("CREATE ROLE jonathan LOGIN;")
-    role = roles['jonathan']
+    role = roles["jonathan"]
 
     assert role.name == "jonathan"
     assert role.login == "LOGIN"
 
-    roles, _ = extract_roles("CREATE ROLE george SUPERUSER CREATEDB INHERIT LOGIN REPLICATION BYPASSRLS CONNECTION LIMIT 3;")
-    role = roles['george']
+    roles, _ = extract_roles(
+        "CREATE ROLE george SUPERUSER CREATEDB INHERIT LOGIN REPLICATION BYPASSRLS CONNECTION LIMIT 3;"
+    )
+    role = roles["george"]
 
     assert role.name == "george"
     assert role.superuser == "SUPERUSER"
@@ -58,31 +65,33 @@ def test_roles():
     assert role.connection_limit == 3
 
     roles, _ = extract_roles("CREATE ROLE davide WITH PASSWORD 'jw8s0F4';")
-    role = roles['davide']
+    role = roles["davide"]
 
     assert role.name == "davide"
     assert role.password == "jw8s0F4"
 
     roles, _ = extract_roles("CREATE ROLE davide WITH PASSWORD '';")
-    role = roles['davide']
+    role = roles["davide"]
 
     assert role.name == "davide"
     assert role.password is None
 
     roles, _ = extract_roles("CREATE ROLE davide WITH PASSWORD NULL;")
-    role = roles['davide']
+    role = roles["davide"]
 
     assert role.name == "davide"
     assert role.password is None
 
     roles, _ = extract_roles("CREATE ROLE davide2 WITH ENCRYPTED PASSWORD 'jw8s0F4';")
-    role = roles['davide2']
+    role = roles["davide2"]
 
     assert role.name == "davide2"
     assert role.password == "jw8s0F4"
 
-    roles, _ = extract_roles("CREATE ROLE miriam WITH LOGIN PASSWORD 'jw8s0F4' VALID UNTIL '2005-01-01';")
-    role = roles['miriam']
+    roles, _ = extract_roles(
+        "CREATE ROLE miriam WITH LOGIN PASSWORD 'jw8s0F4' VALID UNTIL '2005-01-01';"
+    )
+    role = roles["miriam"]
 
     assert role.name == "miriam"
     assert role.login == "LOGIN"
@@ -90,7 +99,7 @@ def test_roles():
     assert role.valid_until == "2005-01-01"
 
     roles, _ = extract_roles("CREATE ROLE admin WITH CREATEDB CREATEROLE;")
-    role = roles['admin']
+    role = roles["admin"]
 
     assert role.name == "admin"
     assert role.createdb == "CREATEDB"
